10cb76ef64479ea221a1cc7cda47acdca9bd76b0
Billy Rennekamp <billy.rennekamp@gmail.com>
2019-01-22 17:06:55 +0100

    front-end basic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1>Talkshop Name Server</h1>
     <router-view/>
+    <div class="footer">
+      <a href="https://github.com/cosmos/sdk-application-tutorial" target="_blank">The Tutorial</a>
+    </div>
   </div>
 </template>
 
@@ -12,12 +15,31 @@ export default {
 </script>
 
 <style>
+body {
+    background: grey;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: rgb(60, 60, 60);
   margin-top: 60px;
+  padding-bottom:150px;
+}
+.footer {
+  position: fixed;
+  bottom:0;
+  left: 0;
+  width:100%;
+  height:150px;
+  line-height:150px;
+  background-color: rgb(60, 60, 60);
+  color: white;
+  text-decoration: none;
+}
+.footer a {
+  color: white;
+  text-decoration: none;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,28 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <textarea spellcheck="false">
+    # query an account
+    $ nscli query account $(nscli keys show YOUR_NAME --address) \
+    --indent --chain-id testchain --node=node.talkshop.name:26657
+
+    # buy a name!
+    $ nscli tx nameservice buy-name SOME_DOMAIN 5mycoin \
+    --from     $(nscli keys show YOUR_NAME --address) \
+    --chain-id testchain --node=node.talkshop.name:26657
+
+    # set a resolver
+    $ nscli tx nameservice set-name SOME_DOMAIN SOME_VALUE\
+    --from     $(nscli keys show YOUR_NAME --address) \
+    --chain-id testchain --node=node.talkshop.name:26657
+
+    # resolve a name
+    $ nscli query nameservice resolve SOME_DOMAIN \
+    --chain-id testchain --node=node.talkshop.name:26657
+
+    # get the whole whois of a name
+    $ nscli query nameservice whois SOME_DOMAIN \
+    --chain-id testchain --node=node.talkshop.name:26657
+    </textarea>
   </div>
 </template>
 
@@ -102,6 +45,17 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
+}
+textarea {
+  text-align: left;
+  width: 100%;
+  max-width:580px;
+  height:420px;
+  font-size:14px;
+  font-family: monospace;
+  padding:20px;
+  line-height:1.4em;
+  color: rgb(60, 60, 60);
 }
 li {
   display: inline-block;
